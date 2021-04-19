df6dc83d535ab9645ac7c523dd492faff711a928
unknown <you@example.com>
2021-04-19 10:57:05 +0300

    Updates

---
+++ static/js/main.5bfb13f8.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpresort=this.webpackJsonpresort||[]).push([[0],{9:function(e,t,r){"use strict";r.r(t);r(0);var n=r(3),s=r.n(n),c=r(1),o=function(){return Object(c.jsx)("div",{children:"Hi"})};s.a.render(Object(c.jsx)(o,{}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.5bfb13f8.chunk.js.map
